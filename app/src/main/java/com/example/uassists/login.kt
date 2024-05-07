5c76a26f65456ef6d0be8035a62547dda98556fa
David-Ramirez-A <79998950+David-Ramirez-A@users.noreply.github.com>
2024-05-07 00:45:21 -0600

    La mayoría de las pantallas

--- app/src/main/java/com/example/uassists/login.kt
+++ app/src/main/java/com/example/uassists/login.kt
@@ -1,20 +1,40 @@
 package com.example.uassists
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class login : AppCompatActivity() {
+    lateinit var btnLogin: Button
+    lateinit var btnVolver: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        btnLogin = findViewById(R.id.btnIngresarLogin)
+        btnVolver = findViewById(R.id.btnVolverAIncio)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, tutorList::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btnVolver.setOnClickListener {
+            val intent = Intent(this, inicio::class.java)
+            startActivity(intent)
+            finish()
+        }
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        }*/
     }
 }
--- app/src/main/java/com/example/uassists/registro.kt
+++ app/src/main/java/com/example/uassists/registro.kt
@@ -1,20 +1,37 @@
 package com.example.uassists
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class registro : AppCompatActivity() {
+class registro : AppCompatActivity()
+{
+    lateinit var btnRegistrarUsuario: Button
+    lateinit var btnVolver: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContentView(R.layout.activity_registro)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        btnRegistrarUsuario = findViewById(R.id.btnRegistrarUsuario)
+        btnVolver = findViewById(R.id.btnVolverALogin)
+
+        btnRegistrarUsuario.setOnClickListener {
+            val intent = Intent(this, registroExitoso::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btnVolver.setOnClickListener {
+            val intent = Intent(this, inicio::class.java)
+            startActivity(intent)
+            finish()
+        }
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+            insets*/
     }
 }

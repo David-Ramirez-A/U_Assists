60673f6c2f05aa6d8cd3055f9d97e794f38b1239
David-Ramirez-A <79998950+David-Ramirez-A@users.noreply.github.com>
2024-06-03 01:50:56 -0600

    Perfil propio
    Ya se carga el perfil propio a partir de la DB

--- app/src/main/java/com/example/uassists/ProfileView.kt
+++ app/src/main/java/com/example/uassists/ProfileView.kt
@@ -1,8 +1,10 @@
 package com.example.uassists
 
+import Controler.UsarioControler
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +12,10 @@ import androidx.core.view.WindowInsetsCompat
 
 class ProfileView : AppCompatActivity() {
     lateinit var btnVolverLista: ImageButton
+    lateinit var lblDatosPerfil: TextView
+
+    //Declaracion del controler donde se comunica con la base de datos y con el modelo
+    private lateinit var usuarioControler: UsarioControler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //Lo siguiente permite que la app se extienda por completo en la pantalla
@@ -20,11 +26,28 @@ class ProfileView : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         btnVolverLista = findViewById(R.id.btnVolverLista)
+        lblDatosPerfil = findViewById(R.id.lblPerfil)
 
         //Recibimiento de la informacion que estaba en la pantalla anterior
         val usuario = intent.getStringExtra("Usuario").toString()
         val tipoUsuario = intent.getStringExtra("tipoUsuario").toString()
+
+        //Inicialización del objeto del controler de usuario
+        usuarioControler = UsarioControler()
+
+        fun cargarPerfilPropio()
+        {
+            val usuarioPerfil = usuarioControler.getUsuario(usuario)
+            val textPerfil = "Nombre: ${usuarioPerfil?.nombre} ${usuarioPerfil?.apellido} \n" +
+                    "Teléfono: ${usuarioPerfil?.telefono} \n" +
+                    "Email: ${usuarioPerfil?.email} \n" +
+                    "Carrera: ${usuarioPerfil?.carrera} \n" +
+                    "Género: ${usuarioPerfil?.genero} \n" +
+                    "Dirección: ${usuarioPerfil?.provincia} ${usuarioPerfil?.canton} ${usuarioPerfil?.distrito}\n"
+            lblDatosPerfil.text = textPerfil
+        }
 
         //Metodos de funcionamiento de los botones
         btnVolverLista.setOnClickListener {
@@ -35,5 +58,7 @@ class ProfileView : AppCompatActivity() {
             finish()
         }
 
+        //Se llama el método para cargar el perfil
+        cargarPerfilPropio()
     }
 }
